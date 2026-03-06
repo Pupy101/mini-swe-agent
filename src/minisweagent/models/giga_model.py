adb99abef9cddda32ccfa89f703ddaa11c671a55
Порхун Сергей Прокофьевич <spporkhun@sdnb-mac-00506.local>
2026-03-06 05:46:59 +0300

    Fix

--- src/minisweagent/models/giga_model.py
+++ src/minisweagent/models/giga_model.py
@@ -67,7 +67,7 @@ class GigaModel:
             "model": self.config.model_name,
             "messages": messages,
             "functions": [BASH_TOOL["function"]],
-            # "function_call": {"name": "bash"},
+            "function_call": {"name": "bash"},
             **(self.config.model_kwargs | kwargs),
         }
 
